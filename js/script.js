f6bba2abf702c50a2755178b5ed0a22eebfbb852
Mary <neur.maria@gmail.com>
2019-07-02 15:40:55 +0200

    CSS preparing for burger menu (before js)

--- js/script.js
+++ js/script.js
@@ -5,16 +5,4 @@
 var name = 'Mary';
 console.log("Welcome to the Portolio Site of");
 console.log(name);
-// Fun with math:
-var nm = 8;
-var age = (nm * 4) - 2;
-console.log(age);
-console.log("years old");
-// taking the quiz with the console calculator
-var x = 100;
-var a = x + 100;
-var b = (x-50)*(x/20);
-var c = x + (x*0.5);
-var d = (x-50)*3;
-console.log(a, b, c, d);
 //
